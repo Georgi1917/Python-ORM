1ba171c6e6524bb934b4086c4ffe3dbcda18e356
Gosho Zvqra <mamarida15@abv.bg>
2023-11-21 23:54:49 +0200

    Added new tasks

--- ExamPrepI/caller.py
+++ ExamPrepI/caller.py
@@ -1,6 +1,6 @@
 import os
 import django
-from django.db.models import Count, Avg
+from django.db.models import Count, Avg, F
 
 # Set up Django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
@@ -61,5 +61,48 @@ def get_top_actor():
     return (f"Top Actor: {top_actor.full_name}, starring in movies: "
             f"{', '.join([m.title for m in top_actor.starring_movies.all()])}, "
             f"movies average rating: {avg_rating:.1f}")
+
+
+def get_actors_by_movies_count():
+    actors = (Actor.objects.
+              prefetch_related("actors_movies").
+              annotate(movies_count=Count("actors_movies")).
+              filter(movies_count__gt=0).order_by("-movies_count", "full_name"))[:3]
+
+    if not actors or not Movie.objects.all():
+        return ""
+
+    actor_info = [
+        f"{a.full_name}, participated in {a.movies_count} movies"
+        for a in actors
+    ]
+
+    return "\n".join(actor_info)
+
+
+def get_top_rated_awarded_movie():
+    top_movie = Movie.objects.filter(is_awarded=True).order_by("-rating", "title").first()
+
+    if not top_movie:
+        return ""
+
+    return (f"Top rated awarded movie: {top_movie.title}, "
+            f"rating: {top_movie.rating:.1f}. "
+            f"Starring actor: {'N/A' if not top_movie.starring_actor else top_movie.starring_actor.full_name}. "
+            f"Cast: {', '.join(a.full_name for a in top_movie.actors.all())}.")
+
+
+def increase_rating():
+    value_to_increase = 0.1
+
+    classic_movies = (Movie.objects.
+                      filter(is_classic=True, rating__lt=10)
+                      .update(rating=F("rating") + value_to_increase))
+
+    if not classic_movies:
+        return "No ratings increased."
+
+    return f"Rating increased for {classic_movies} movies."
+
 
 
